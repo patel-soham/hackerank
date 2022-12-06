5e5e7a187980ca43a943e4655e4e70b917051f19
Soham Patel <40416883+patel-soham@users.noreply.github.com>
2022-12-06 10:38:55 -0500

    Create VectorSort.cpp

---
+++ c++ by topics/VectorSort.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */       int n;
+    cin>>n; // collect size of array
+    
+    vector<int> arr(n);
+
+    //WAY 1
+    for (auto i=0; i<arr.size(); i++){
+        cin>>arr[i];
+    }
+    /*
+    WAY 2
+    for (auto& i : arr){
+        cin>>i;
+    }*/
+    sort(arr.begin(),arr.end()); //ascending sort
+    
+    // WAY 3
+    for (auto j=arr.begin(); j<arr.end(); j++){
+        cout<<*j<<" ";
+    }
+    
+    return 0;
+}
